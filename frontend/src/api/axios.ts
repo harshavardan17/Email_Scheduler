e69ff4469b66170e0ec6d7d0e2b94c727645e0cb
Harsha Alikepalli <alikepalliharsha8951@gmail.com>
2026-08-07 19:12:56 +0530

    Use Vercel API URL

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://email-scheduler-8rr9.onrender.com/api",
 });
 
 export default api;
